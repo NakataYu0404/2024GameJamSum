4bb67b089f614c784ada067632f7aa677beebdec
kohei126 <158240582+kohei126@users.noreply.github.com>
2024-08-09 10:40:21 +0900

    READY状態の時のカウントダウン実装

--- Src/Scene/GameScene.cpp
+++ Src/Scene/GameScene.cpp
@@ -65,6 +65,13 @@ void GameScene::Init(void)
 
 	magma_ = std::make_shared<Magma>();
 	magma_->Init();
+
+	Timer::GetInstance().ResetCountDownTimer();
+
+	player1ReadyFlag_ = false;
+	player2ReadyFlag_ = false;
+	player3ReadyFlag_ = false;
+	player4ReadyFlag_ = false;
 }
 
 void GameScene::Update(void)
@@ -127,6 +134,7 @@ void GameScene::DrawUI(void)
 	switch (inTypeGame_)
 	{
 	case GameScene::InSceneType::READY:
+		Timer::GetInstance().CountDownDraw();
 		break;
 	case GameScene::InSceneType::INGAME:
 		Timer::GetInstance().Draw();
@@ -145,15 +153,43 @@ void GameScene::UpdateReady(void)
 {
 	auto& input = InputManager::GetInstance();
 
-	if (input.IsTrgDown(KEY_INPUT_SPACE) ||
-		(input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1,InputManager::JOYPAD_BTN::DOWN)&&
-		input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD2, InputManager::JOYPAD_BTN::DOWN)&&
-		input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD3, InputManager::JOYPAD_BTN::DOWN)&&
-		input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD4, InputManager::JOYPAD_BTN::DOWN)))
+	if (input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1, InputManager::JOYPAD_BTN::DOWN))
+	{
+		player1ReadyFlag_ = true;
+	}
+	if (input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD2, InputManager::JOYPAD_BTN::DOWN))
+	{
+		player2ReadyFlag_ = true;
+	}
+	if (input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD3, InputManager::JOYPAD_BTN::DOWN))
+	{
+		player3ReadyFlag_ = true;
+	}
+	if (input.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD4, InputManager::JOYPAD_BTN::DOWN))
+	{
+		player4ReadyFlag_ = true;
+	}
+	if (input.IsTrgDown(KEY_INPUT_SPACE))
+	{
+		player1ReadyFlag_ = true;
+		player2ReadyFlag_ = true;
+		player3ReadyFlag_ = true;
+		player4ReadyFlag_ = true;
+	}
+
+	if(
+		player1ReadyFlag_ && 
+		player2ReadyFlag_ &&
+		player3ReadyFlag_ && 
+		player4ReadyFlag_
+		)
 	{
 		//	TODO:ここにカウントダウン演出を追加して、カウントダウンが終わったらinTypeGameが変わるようにする
-		inTypeGame_ = InSceneType::INGAME;
-		Timer::GetInstance().ResetTimer();
+  		if (Timer::GetInstance().CountDown(5.0f))
+		{
+			inTypeGame_ = InSceneType::INGAME;
+			Timer::GetInstance().ResetTimer();
+		}
 	}
 }
 
